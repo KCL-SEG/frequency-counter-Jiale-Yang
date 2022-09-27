3a52a4e6dd3f1769eed87cf085eddd52ff17850a
Jiale Yang <jiale.2.yang@kcl.ac.uk>
2022-09-27 10:03:31 +0100

    frequencies

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,12 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
-    frequencies = {}
-    # Your code goes here
-    return frequencies
+        
+        frequencies = {}
+	
+        for item in items:
+                frequencies[str(item)] = frequencies.get(str(item), 0) + 1	
+	
+
+
+        return frequencies	
